86350a869cd31326650119d039522c7f6c9aff52
PanuwatYathuam <panuwat_ya67@live.rmutl.ac.th>
2025-08-17 18:35:51 +0700

    1

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,18 +1,21 @@
-export const projects = [
-  {
-    id: 1,
-    title: "weather-app",
-    image: "/images/WeatherApp.jpg",
-    demoUrl: "https://www.figma.com/board/szqMZOmtfWrG6rxtOwbPx4/Week4--JavaScript-Workshop?node-id=709-446&t=471UdQX08PHz1CaQ-0",
-    githubUrl: "https://github.com/PanuwatYathuam/my-weather-app",
-    featured: true
-  },
-  {
-    id: 2,
-    title: "Weather App",
-    image: "/images/to-bo list.jpg",
-    demoUrl: "https://lab3-reactjs-fundamentals-experiment2.netlify.app/",
-    githubUrl: "https://github.com/PanuwatYathuam/react-workshop",
-    featured: false
-  },
+import weatherAppImage from '../images/WeatherApp.jpg';
+import todoListImage from '../images/to-bo list.jpg';
+
+export const projects = [ 
+    { 
+        id: 1, 
+        title: "weather-app", 
+        image: weatherAppImage, 
+        demoUrl: "https://www.figma.com/board/szqMZOmtfWrG6rxtOwbPx4/Week4--JavaScript-Workshop?node-id=709-446&t=471UdQX08PHz1CaQ-0", 
+        githubUrl: "https://github.com/PanuwatYathuam/my-weather-app", 
+        featured: true 
+    }, 
+    { 
+        id: 2, 
+        title: "Weather App", 
+        image: todoListImage, 
+        demoUrl: "https://lab3-reactjs-fundamentals-experiment2.netlify.app/", 
+        githubUrl: "https://github.com/PanuwatYathuam/react-workshop", 
+        featured: false 
+    }, 
 ];
